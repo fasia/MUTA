c97871893fd86d88165a2b76aa07235232bd8294
fasia <fasiava@gmail.com>
2017-04-05 17:04:21 +0300

    CTS added
    CTS added

--- mut_opt.py
+++ mut_opt.py
@@ -56,16 +56,18 @@ def main(argv):
     global tree, root
     tree= ET.parse(inputfile)
     root = tree.getroot()
-    print(root.tag)
+    #print(root.tag)
     NewDir (inputfile)
-    CN(inputfile[:-4],templatename,queryfile)
+    #CN(inputfile[:-4],templatename,queryfile)
     #CS(inputfile[:-4], templatename,queryfile)
     #CT(inputfile[:-4], templatename,queryfile)
     #CG(inputfile[:-4], templatename,queryfile)
     #NG(inputfile[:-4],templatename,queryfile)
     #C_I(inputfile[:-4],templatename,queryfile)
     # IR(inputfile[:-4],templatename,queryfile)
-
+    #########--------------------------------------------HOM----------------------------------------------------#######
+    #merging CT and CS
+    CTS(inputfile[:-4], templatename, queryfile)
 def NewDir(i):
     print('in NewDir')
     if not os.path.exists('Mutants_'+i[:-4]+'_Valid'):
@@ -162,70 +164,74 @@ def CT(inp, tem,que):#change Target of transition
                                     os.remove(MyName)
                                     #print 'it is deleted'
 
-def CN(inp,tem,que): # change output transition name
-    i=0
-    j=0
-    # get the transitions in the target template
+def CN(inp, tem, que):  # change output transition
+    i = 0
+    j = 0
     for temp in root.findall('template'):
-        if temp.find('name').text==tem:
-            listOfTransitions= []
-            for tran in temp.findall('transition'):
-                sync =tran.find("label[@kind='synchronisation']") # gets all synchronizations
-                if sync!= None:
-                    listOfTransitions.append(sync.text)
-                # else:
-                #     listOfTransitions.append(None)
-            print("listOfTransitions", listOfTransitions)
+        if temp.find('name').text == tem:
+            r = []
+            for loc in temp.findall('transition'):
+                sync = loc.find("label[@kind='synchronisation']")
+                if sync != None:
+                    r.append(sync.text)
+                else:
+                    r.append(None)
+            #print("transitions are: ",r)
             # r[loc] = [loc.find("label[@kind='synchronisation']").text for loc in temp.findall('transition')]
-            #print 'main temp', temp.find('name').text
-            # #print 'all transitions are',r
-            for output_selection in range(len(listOfTransitions)):
-                check= listOfTransitions[output_selection]
-                #print check
-                if check[-1:]!= '!':
-                    listOfTransitions[output_selection]=None # remove output synch
-                else: j+=1 # counting the number of output transitions
-            # #print 'all output transitions ',r, j
-            for ii in range(len(listOfTransitions)):
-                for k in range(len(listOfTransitions)):
-                    strin = 'transition['+str(k)+']'
-                    #print("stin", strin)
-                    MyName=inp+'MUT_CN'+str(k)+'_'+str(ii)+'.xml'
+            #print('The template is:', temp.find('name').text)
+            # print 'all transitions are',r
+            for output_selection in range(len(r)):
+                check = r[output_selection]
+                #print("current transition is: ",check)
+                if check != None and check[-1:] != '?':
+                    r[output_selection] = None
+                # else:
+                #     j += 1  # counting the number of output transitions
+            #print("now transiotions are:", r, "and they are: ", j)
+            for ii in range(len(r)):
+                for k in range(len(r)):
+                    #strin = 'transition[' + str(k) + ']'
+                    MyName = inp + 'MUT_CN' + str(k) + '_' + str(ii) + '.xml'
                     tree.write(MyName)
                     treex = ET.parse(MyName)
                     rootx = treex.getroot()
-                    # for all templates in the model it looks for the target template and changes the names one by one
                     for t in rootx.findall('template'):
-                        if t.find('name').text==tem:
-                            #print('t ',t.findall("label[@kind='synchronisation']"))
-                            #print("strin",strin)
-                            #strin = 'transition['+str(k)+']'
-                            transitionsList = t.findall("./transition/label[@kind='synchronisation']")
-                            print(transitionsList[k].text)
-                            transitionText= transitionsList[k].text
-                            if transitionText[-1:]=='!':
-                                print("inside if")
-                                if transitionText != listOfTransitions[ii] and listOfTransitions[ii]!= None:
-                                    print(t.find("./transition/label[@kind='synchronisation']").text)#=listOfTransitions[ii]
-                                    #print 'tarn name ',ss,'changes to', s.text
-                                    treex.write(inp+'MUT_CN'+str(k)+'_'+str(ii)+'.xml')
-                                    i+=1
-                                    #print MyName,'new tree is made for transitions',s.text
-                                    if CheckQuery(MyName):
-                                        Change_dir(MyName,'yes')
-                                    else:
-                                        Change_dir(MyName,'no')
-                                else:
-                                    os.remove(inp+'MUT_CN'+str(k)+'_'+str(ii)+'.xml')
-                                    # #print MyName,'it is deleted'
-                            else:
-                                os.remove(inp+'MUT_CN'+str(k)+'_'+str(ii)+'.xml')
+                        if t.find('name').text == tem:
+            #                print(strin)
+                            strin = 'transition[' + str(k) + ']'
+                            tra = t.find(strin)
+                            s = tra.find("label[@kind='synchronisation']")
+                            if s != None:
+             #                   print('transition is ', s.text)
 
-                        else:
-                            #print 'remove the unsync channels'
-                            os.remove(MyName)
-                            #print MyName,'it is deleted'
-                            #print 'i is :', i
+                                ss = s.text
+                                if ss[-1:] == '?':
+                                    # print 'inside '
+                                    if ss != r[ii] and r[ii] != None:
+                                        s.text = r[ii]
+              #                          print('tarn name ', ss, 'changes to', s.text)
+                                        treex.write(inp + 'MUT_CN' + str(k) + '_' + str(
+                                            ii) + '.xml')
+                                        i += 1
+               #                         print(MyName, 'new tree is made for transitions', s.text)
+               #                          if CheckQuery(MyName):
+               #                              Change_dir(MyName, 'yes')
+               #                          else:
+               #                              Change_dir(MyName, 'no')
+                                    else:
+                                        os.remove(inp + 'MUT_CN' + str(k) + '_' + str(
+                                            ii) + '.xml')
+                                        # print MyName,'it is deleted'
+                                else:
+                                    os.remove(
+                                        inp + 'MUT_CN' + str(k) + '_' + str(ii) + '.xml')
+
+                            else:
+                #                print('remove the unsync channels')
+                                os.remove(MyName)
+
+                 #               print(MyName, 'it is deleted')
+    #print('i is :', i)
 
 def CheckQuery(MyName):
     try:
@@ -390,9 +396,6 @@ def SL(): # has problem
                             else:
                                 Change_dir(MyName,'no')
 
-
-
-
 #Invert reset
 def IR(inp,tem,que):
     for t in root.findall('template'):
@@ -432,20 +435,52 @@ def IR(inp,tem,que):
                         # else:
                         #     os.remove(MyName)
 
-# NewDir()
-# CN()
-# CS()
-# CT()
-# CG()
-# NG()
-# CI()
-# SL()
-# IR()
-# CheckQuery()
+def CTS(inp, tem, que):
+    for t in root.findall('template'):
+        r =  [loc.attrib['id'] for loc in t.findall('location')]
+        r2 = [loc.find('label') for loc in t.findall('transition')]
+        #print 'number of transitions:', len(r),r, len(r2), r2
+        #print 'main temp', t.find('name').text
+        if t.find('name').text==tem:
+            for ii in range(len(r)):
+                for k in range(len(r2)):
+                    strin = 'transition['+str(k)+']'
+                    MyName=inp+'MUT_CTS'+str(k)+'_'+str(ii)+'.xml'
+                    tree.write(MyName)
+                    treex = ET.parse(MyName)
+                    rootx = treex.getroot()
+                    for t in rootx.findall('template'):
+                        #print 'inside temp', t.find('name').text
+                        if t.find('name').text==tem:
+                            #print strin
+                            strin = 'transition['+str(k)+']'
+                            tra = t.find(strin)
+                            #print 'in Template and transition is',tra.find('label').text
 
-# current_path='\FSDT\Mutation_UPTA\mut_locations'
-# ChangeDIR()
-
+                            for s in tra:
+                                target = s.iter('target')
+                                source = s.iter('source')
+                                print "Before: target", target,"source", source
+                                if target != source: # not a loop edge
+                                    t=target.attrib['ref']
+                                    target.attrib['ref'] = source.attrib['ref']
+                                    source.attrib['ref']= t
+                                print "target", target,"source", source
+                            # for s in tra.iter('target'):
+                            #     #print 'with source location',s.attrib,'candidate location is : ',r[ii]
+                            #     before= s.attrib['ref']
+                            #     if before != r[ii]:
+                            #         s.attrib['ref']=r[ii]
+                            #         #print 'location',before,'changes to', s.attrib
+                            #         treex.write(MyName)
+                            #         #print 'new tree is made'
+                            #         if CheckQuery(MyName):
+                            #             Change_dir(MyName,'yes')
+                            #         else:
+                            #             Change_dir(MyName,'no')
+                            #     else:
+                            #         os.remove(MyName)
+                                    #print 'it is deleted'
 
 if __name__ == "__main__":
     main(sys.argv[1:])
